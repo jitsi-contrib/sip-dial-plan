0fec52c221ec21a06324f9cfe53f294425925f52
emrah <emrah@emrah.com>
2022-09-13 19:47:21 +0300

    refactor: notes into config

--- config.ts
+++ config.ts
@@ -1,2 +1,6 @@
+// check your prosody config file and get the secret from "app_secret"
 export const TOKEN_SECRET = "myappsecret";
+
+// "HS256" and "HS512" are supported. If you don't update it explicitly in your
+// prosody config file then the default value is "HS256"
 export const TOKEN_ALGORITHM = "HS256";
